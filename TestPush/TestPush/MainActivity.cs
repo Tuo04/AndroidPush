c5a1f7c4b24e375f248fd75cff42ec0715531eaf
Tuo Chen (Beyondsoft Corporation) <v-tuchen@microsoft.com>
2017-06-09 01:02:34 -0700

    test push

--- TestPush/TestPush/MainActivity.cs
+++ TestPush/TestPush/MainActivity.cs
@@ -16,7 +16,6 @@ namespace TestPush
     [Activity(Label = "TestPush", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        TextView msgText;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -24,32 +23,9 @@ namespace TestPush
             // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
 
-            MobileCenter.Start("f35f396a-e513-440b-b8f3-1d159412b139",
+            MobileCenter.Start("a23996ab-2d7c-4f76-a847-06b3e580648b",
                    typeof(Analytics), typeof(Crashes), typeof(Push));
-
-            msgText = FindViewById<TextView>(Resource.Id.msgText);
-            IsPlayServicesAvailable();
-        }
-        public bool IsPlayServicesAvailable()
-        {
-            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
-                             
-            if (resultCode != ConnectionResult.Success)
-            {
-                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
-                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
-                else
-                {
-                    msgText.Text = "This device is not supported";
-                    Finish();
-                }
-                return false;
-            }
-            else
-            {
-                msgText.Text = "Google Play Services is available.";
-                return true;
-            }
+            Push.Enabled = true;
         }
     }
 }
