c3aa7d974b627f8e61b8cb413438289ec1b0ca59
Tuo Chen (Beyondsoft Corporation) <v-tuchen@microsoft.com>
2017-06-16 01:19:08 -0700

    test

--- TestPush/TestPush/MainActivity.cs
+++ TestPush/TestPush/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Widget;
 using Android.OS;
 using Android.Gms.Common;
+using Android.Content;
 using Android.Gms.Common.Apis;
 using Firebase.Messaging;
 using Firebase.Iid;
@@ -16,15 +17,20 @@ namespace TestPush
     [Activity(Label = "TestPush", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        TextView msgText;
+
         protected override void OnCreate(Bundle bundle)
         {
             //MobileCenter.SetLogUrl("https://in-staging-south-centralus.staging.avalanch.es");
 
-            // This should come before MobileCenter.Start() is called
-            Push.PushNotificationReceived += (sender, e) => {
+            SetContentView(Resource.Layout.Main);
+
+            //// This should come before MobileCenter.Start() is called
+            Push.PushNotificationReceived += (sender, e) =>
+            {
 
                 // Add the notification message and title to the message
-                var summary = $"Push notification received:"+$"\n\tNotification title: {e.Title}" +
+                var summary = $"Push notification received:" + $"\n\tNotification title: {e.Title}" +
                             $"\n\tMessage: {e.Message}";
 
                 // If there is custom data associated with the notification,
@@ -38,22 +44,41 @@ namespace TestPush
                     }
                 }
 
-                    // Send the notification summary to debug output
-                    System.Diagnostics.Debug.WriteLine(summary);
+                // Send the notification summary to debug output
+                System.Diagnostics.Debug.WriteLine(summary);
             };
-
-            string temp=MobileCenter.InstallId.ToString();
 
             Push.Enabled = true;
 
             base.OnCreate(bundle);
+            msgText = FindViewById<TextView>(Resource.Id.msgText);
 
-            // Set our view from the "main" layout resource
-            SetContentView (Resource.Layout.Main);
+            IsPlayServicesAvailable();
+
             Push.EnableFirebaseAnalytics();
             MobileCenter.Start("a6c3e682-15b7-4925-9386-1e39881a8fd3", 
                    typeof(Analytics), typeof(Crashes), typeof(Push));
             
+        }
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                else
+                {
+                    msgText.Text = "This device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                msgText.Text = "Google Play Services is available.";
+                return true;
+            }
         }
     }
 }
