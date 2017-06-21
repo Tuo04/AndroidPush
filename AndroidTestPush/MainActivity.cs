67d9cba56317a89ec86b085243bf6955ec48c744
Tuo04 <chentuo01@beyondsoft.com>
2017-06-20 23:41:38 -0700

    New Sulotion

---
+++ AndroidTestPush/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Microsoft.Azure.Mobile.Push;
+
+namespace AndroidTestPush
+{
+    [Activity(Label = "AndroidTestPush", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Push.Enabled = true;
+
+            // This should come before MobileCenter.Start() is called
+            Push.PushNotificationReceived += (sender, e) => {
+
+                // Add the notification message and title to the message
+                var summary = $"Push notification received:"+
+                                    $"\n\tNotification title: {e.Title}" +
+                        $"\n\tMessage: {e.Message}";
+
+                // If there is custom data associated with the notification,
+                // print the entries
+                if (e.CustomData != null)
+                {
+                    summary += "\n\tCustom data:\n";
+                    foreach (var key in e.CustomData.Keys)
+                    {
+                        summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                    }
+                }
+
+                // Send the notification summary to debug output
+                System.Diagnostics.Debug.WriteLine(summary);
+            };
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            MobileCenter.Start("c9e67f6d-f96b-41d2-9628-e7a2b96dea66",
+                   typeof(Analytics), typeof(Crashes), typeof(Push));
+        }
+    }
+}
+
